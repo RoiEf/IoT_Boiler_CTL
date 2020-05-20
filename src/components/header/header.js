bb4f019ffa3b63af6f0db3378b7c7ddc0e557c30
RoiEf <45352222+RoiEf@users.noreply.github.com>
2020-05-20 13:15:28 +0300

    Revert "sync"

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -10,24 +10,17 @@ const Header = (/* props */) => {
     <header id="header">
       <h1>Preact App with Microbundle</h1>
       <nav>
-        <Link href="/">
-          Home
-        </Link>
+        <Link href="/">Home</Link>
         {/* part of the program {state.isAutenticated ? (
 			<Fragment> 
          <Link href="/iot">
           IoT Specifics
         </Link> */}
-        <Link href="/network">
-          Network
-        </Link>
-        <Link href="/updates">
-          Updates
-        </Link>
+        <Link href="/basic">Basic Settings</Link>
+        <Link href="/wifi">WiFi Settings</Link>
+        <Link href="/updates">Updates</Link>
         {/* part of the program </Fragment>):( */}
-        <Link href="/login">
-          Login
-        </Link>
+        <Link href="/login">Login</Link>
         {/* part of the program )} */}
       </nav>
     </header>
---
+++ src/components/save/save.js
@@ -0,0 +1,12 @@
+import { h } from "preact";
+// import { useState, useEffect, useContext } from "preact/hooks";
+
+const Save = () => {
+  return (
+    <p class="submit">
+      <input type="submit" name="commit" value="Save" />
+    </p>
+  );
+};
+
+export default Save;
