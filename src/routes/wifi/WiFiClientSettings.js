84d5c75e5c1b57d29735001bbd5c4b57330f1269
Roi <roi.efrati@gmail.com>
2020-09-14 11:43:41 +0300

    cleanup

--- src/routes/wifi/WiFiClientSettings.js
+++ src/routes/wifi/WiFiClientSettings.js
@@ -1,32 +1,8 @@
 import { h } from "preact";
-// import { useState, useEffect } from "preact/hooks"; //  , useContext
-// import { route } from 'preact-router';
-// import { StateContext } from "../../context/stateContext";
-// import useWifi from "../../context/useWifi";
-
-// import Save from "../../components/save/save";
-// const address = `http://${window.location.hostname}:${80}/network`;
 import UseDhcp from "./dhcp";
 import StaticIP from "./staticIP";
 
 const WiFiClientSettings = (/* props */) => {
-  // const [dhcp, dhcpToggle] = useState(true);
-  // const { getIPData, saveIPData } = useWifi();
-  // const [ssid, updateSSID] = useState("base_iot");
-  // const [password, updatePassword] = useState("");
-  // const [state, setState] = useContext(StateContext);
-
-  // const onSubmit = (e) => {
-  //   e.preventDefault();
-  //   saveIPData({ /* SSID: ssid, wifiPassword: password */ });
-  // };
-
-  // useEffect(() => {
-  //   console.log("wifi.js get data");
-  //   getIPData();
-  //   // updateSSID(SSID);
-  //   // updatePassword(wifiPassword);
-  // }, [/*SSID, wifiPassword*/]);
 
   return (
     <div id="contentBox">
