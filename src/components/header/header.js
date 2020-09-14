ff2d9edddd2f46dbc3226622da05281ee797ae29
Roi <roi.efrati@gmail.com>
2020-09-14 16:56:16 +0300

    reinstated show/hide login in header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,28 +1,27 @@
-import { h /*, Fragment */ } from "preact";
+import { h, Fragment } from "preact";
 import { Link } from "preact-router/match";
-// import { useContext } from 'preact/hooks';
+import { useContext } from 'preact/hooks';
+import { StateContext } from "../../context/stateContext";
+
 // import { AuthContext } from "../../context/authContext";
 
 const Header = (/* props */) => {
-  // const [state, setState] = useContext(AuthContext);
+  const [state, setState] = useContext(StateContext);
 
   return (
     <header id="header">
       <h1>Preact App with Microbundle</h1>
       <nav>
         <Link href="/">Home</Link>
-        {/* part of the program {state.isAutenticated ? (
-			<Fragment> 
-         <Link href="/iot">
-          IoT Specifics
-        </Link> */}
-        <Link href="/basic">Basic Settings</Link>
-        <Link href="/wifi">WiFi Settings</Link>
-        <Link href="/updates">Updates</Link>
-        <Link href="/admin">Admin</Link>
-        {/* part of the program </Fragment>):( */}
-        <Link href="/login">Login</Link>
-        {/* part of the program )} */}
+        {state.auth.isAutenticated ? (<Fragment>
+          {/*  
+          <Link href="/iot">IoT Specifics</Link>
+          <Link href="/basic">Basic Settings</Link>
+         */}
+          <Link href="/wifi">WiFi Settings</Link>
+          <Link href="/updates">Updates</Link>
+          <Link href="/admin">Admin</Link>
+        </Fragment>) : (<Link href="/login">Login</Link>)}
       </nav>
     </header>
   );
