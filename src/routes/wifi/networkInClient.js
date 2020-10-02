028b64fb5d9ff4c6c675a90fe4e52456cc0c1787
RoiEf <roi.efrati@gmail.com>
2020-10-02 23:18:50 +0300

    Night Sync

---
+++ src/routes/wifi/networkInClient.js
@@ -0,0 +1,20 @@
+import { h, Fragment } from "preact";
+import { useState, useEffect } from "preact/hooks";
+
+const SSIDinSTA = () => {
+  const [ssid, updateSSID] = useState("");
+  const [authType, updateAuthType] = useState("");
+
+  return (
+    <Fragment>
+      <p>SSID in Client mode:</p>
+      <p>
+        Network SSID: {ssid ? ssid : <b>No Network selected</b>}
+        {ssid && " | Autentication: "}
+        {ssid && authType ? authType : <b>Open Network</b>}
+      </p>
+    </Fragment>
+  );
+};
+
+export default SSIDinSTA;
