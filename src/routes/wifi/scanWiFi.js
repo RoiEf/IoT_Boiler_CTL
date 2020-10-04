a1a8438704f1b65ee45e66119a07fcb62a7ef07d
RoiEf <roi.efrati@gmail.com>
2020-10-04 22:46:54 +0300

    EoD save

--- src/routes/wifi/scanWiFi.js
+++ src/routes/wifi/scanWiFi.js
@@ -1,20 +1,34 @@
 import { h, Fragment } from "preact";
 import { useState, useEffect } from "preact/hooks";
+import ScanWiFiLines from "./scanWiFiLines";
 
-const ScanWiFi = () => {
-  const [ssid, updateSSID] = useState("");
-  const [authType, updateAuthType] = useState("");
+function ScanWiFi() {
+  const [found, updateFound] = useState(false);
+  const [LinesArr, updateLinesArr] = useState([]);
 
   return (
     <Fragment>
-      <p>SSID in Client mode:</p>
-      <p>
-        Network SSID: {ssid ? ssid : <b>No Network selected</b>}
-        {ssid && " | Autentication: "}
-        {ssid && authType ? authType : <b>Open Network</b>}
+      <p>WiFi networks in range</p>
+      {found ? (
+        <p>
+          <table>
+            <th>
+              <td> </td>
+              <td>SSID</td>
+              <td>Authentication</td>
+              <td>Signal strength</td>
+            </th>
+            <ScanWiFiLines LinesArr={LinesArr} />
+          </table>
+        </p>
+      ) : (
+        <p>NO WiFi networks Found</p>
+      )}
+      <p class="submit">
+        <input type="submit" name="rescan" value="Scan Again" />
       </p>
     </Fragment>
   );
-};
+}
 
 export default ScanWiFi;
---
+++ src/routes/wifi/scanWiFiLines.js
@@ -0,0 +1,10 @@
+import { h, Fragment } from "preact";
+// import { useState, useEffect } from "preact/hooks";
+
+function ScanWiFiLines(LinesArr) {
+  // const [found, updateFound] = useState(false);
+  // const [authType, updateAuthType] = useState("");
+  return <Fragment></Fragment>;
+}
+
+export default ScanWiFiLines;
