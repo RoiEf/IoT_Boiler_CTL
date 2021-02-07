ecc2f9fcb9a0697a09b22861688cc4aa01a9262a
RoiEf <roi.efrati@gmail.com>
2021-02-07 22:16:33 +0200

    Cleanup

--- src/routes/wifi/scanWiFi.js
+++ src/routes/wifi/scanWiFi.js
@@ -1,6 +1,5 @@
 import { h, Fragment } from "preact";
 import { useState, useEffect } from "preact/hooks";
-// import ScanWiFiLines from "./scanWiFiLines";
 import useWifi from "../../context/useWifi";
 
 const address = `http://${window.location.hostname}:${80}/wifi/scan`;
@@ -29,30 +28,6 @@ function ScanWiFi() {
     updateFound(wifiLinesUpdate);
   }, [wifiLinesArr, wifiLinesUpdate]);
 
-  // const updateData = () => {
-  //   console.log("scanWiFi.js updateData");
-  //   fetch(address, {
-  //     method: "post",
-  //     headers: {
-  //       "Content-type": "application/json",
-  //     },
-  //     body: JSON.stringify({
-  //       userName: "admin",
-  //       password: "12345",
-  //     }),
-  //   })
-  //     .then((res) => res.json())
-  //     .then((res) => {
-  //       // console.log("res: ", res);
-  //       if (res.message === "No Networks found") {
-  //         updateFound(false);
-  //       } else {
-  //         updateLinesArr(res.networks);
-  //         updateFound(true);
-  //       }
-  //     })
-  //     .catch((error) => console.log("something failed", error));
-  // };
 
   const onSubmitScanAgain = (e) => {
     e.preventDefault();
